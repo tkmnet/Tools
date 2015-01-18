61546b26ab883c8b7911f47d21797cd1b6018743
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2015-01-19 00:13:58 +0900

    update jdkDL.sh

--- jdkDL.sh
+++ jdkDL.sh
@@ -6,3 +6,7 @@ if [ $# -eq 1 ]; then
 fi
 
 wget -O /tmp/jdk${DL_JAVA_VER}.tar.gz --no-check-certificate --no-cookies - --header "Cookie: oraclelicense=accept-securebackup-cookie" `wget -q -O - http://www.oracle.com/technetwork/java/javase/downloads/index.html | grep -o "\/technetwork\/java/\javase\/downloads\/jdk${DL_JAVA_VER}-downloads-[0-9]*\.html" | head -1 | xargs -I@ echo "http://www.oracle.com"@ | xargs wget -q -O - | grep -o "http.*jdk-${DL_JAVA_VER}u[0-9]*-linux-x64.tar.gz" | head -1`
+exit
+
+# one-line script
+# sh -c 'DL_JAVA_V=8;wget -O /tmp/jdk${DL_JAVA_VER}.tar.gz --no-check-certificate --no-cookies - --header "Cookie: oraclelicense=accept-securebackup-cookie" `wget -q -O - http://www.oracle.com/technetwork/java/javase/downloads/index.html | grep -o "\/technetwork\/java/\javase\/downloads\/jdk${DL_JAVA_VER}-downloads-[0-9]*\.html" | head -1 | xargs -I@ echo "http://www.oracle.com"@ | xargs wget -q -O - | grep -o "http.*jdk-${DL_JAVA_VER}u[0-9]*-linux-x64.tar.gz" | head -1`'
