e8b1a4096a584a5a53fbd314a6ff65fc2d5acc57
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2015-03-09 07:00:53 +0900

    update install-aipo.sh

--- install-aipo.sh
+++ install-aipo.sh
@@ -13,13 +13,13 @@ sudo tar xvzf aipo7020aja_linux/aipo7020.tar.gz
 sudo rm -rf aipo7020aja_linux
 
 sudo apt-get update
-sudo apt-get -y install build-essential zlib1g-dev libreadline-gplv2-dev nmap
+sudo apt-get -y install build-essential gcc-4.7 zlib1g-dev libreadline-gplv2-dev nmap
 
 cd /usr/local/aipo/bin
 sudo sed -iE 's/^sh /bash /' installer.sh
 sudo sed -iE 's%^rpm.*$%dpkg -l > ${script_path}/bin/rpmlist%' utf8/installer.sh
 sudo sed -iE 's%^tmp_str.*rpmlist.*$%tmp_str="THROW"%' utf8/installer.sh
-sudo sed -iE 's%configure%configure CFLAGS="-O1"%' utf8/installer.sh
+sudo sed -iE 's%configure%configure CC="gcc-4.7" CFLAGS="-O1"%' utf8/installer.sh
 
 sudo sh installer.sh
 
