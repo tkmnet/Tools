606eed2cb1fb92df9825d9860c05b7ac24752acb
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2015-03-09 01:35:56 +0900

    update install-aipo.sh

--- install-aipo.sh
+++ install-aipo.sh
@@ -3,7 +3,7 @@
 
 cd /tmp
 
-wget http://sourceforge.jp/frs/redir.php?m=iij&f=/aipo/60038/aipo7020aja_linux64.tar.gz
+wget "http://sourceforge.jp/frs/redir.php?m=iij&f=/aipo/60038/aipo7020aja_linux64.tar.gz"
 tar xf aipo7020aja_linux64.tar.gz
 
 cd aipo7020aja_linux64
