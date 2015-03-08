ca8df4fe5b8a129b77c3afebd734e0dfafde9ec7
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2015-03-09 01:46:50 +0900

    update install-aipo.sh

--- install-aipo.sh
+++ install-aipo.sh
@@ -3,8 +3,10 @@
 
 cd /tmp
 
-# wget -O aipo7020aja_linux64.tar.gz 'http://sourceforge.jp/frs/redir.php?m=iij&f=/aipo/60038/aipo7020aja_linux64.tar.gz'
-# tar xf aipo7020aja_linux64.tar.gz
+wget -O aipo7020aja_linux64.tar.gz 'http://sourceforge.jp/frs/redir.php?m=iij&f=/aipo/60038/aipo7020aja_linux64.tar.gz'
+tar xf aipo7020aja_linux64.tar.gz
 
 cd aipo7020aja_linux64
 
+apt-get update
+apt-get -y install make gcc libreadline-dev libghc-zlib-dev libghc-pipes-zlib-dev lua-zlib-dev nmap
