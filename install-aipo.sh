f774a2d30c49d3352ef31d0b36474db7c82eb9f7
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2015-03-09 02:04:41 +0900

    update install-aipo.sh

--- install-aipo.sh
+++ install-aipo.sh
@@ -4,9 +4,9 @@
 cd /usr/local
 sudo cat /dev/null
 
-wget -O aipo7020aja_linux64.tar.gz 'http://sourceforge.jp/frs/redir.php?m=iij&f=/aipo/60038/aipo7020aja_linux64.tar.gz'
-tar xzf aipo7020aja_linux64.tar.gz
-rm -f aipo7020aja_linux64.tar.gz
+sudo wget -O aipo7020aja_linux64.tar.gz 'http://sourceforge.jp/frs/redir.php?m=iij&f=/aipo/60038/aipo7020aja_linux64.tar.gz'
+sudo tar xzf aipo7020aja_linux64.tar.gz
+sudo rm -f aipo7020aja_linux64.tar.gz
 sudo tar xzf aipo7020aja_linux/aipo7020.tar.gz
 sudo rm -rf aipo7020aja_linux
 
