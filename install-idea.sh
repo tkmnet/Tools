d4e00d0bdaf3003812d1b8ccb1087351c89b982c
Shunki TAKAMI(takamin@sylph.maslab) <maslab@takamin.net>
2015-04-09 17:35:13 +0900

    update

--- install-idea.sh
+++ install-idea.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 
-sudo cat /dev/null
+sudo ls
 cd /tmp
 rm -rf idea
 mkdir idea
 cd idea
 wget -O /tmp/idea/idea.tar.gz `wget -q -O - https://www.jetbrains.com/idea/download/download_thanks.jsp --post-data "os=linux&edition=IC" | grep -o "https:\/\/.*\.tar\.gz" | head -1`
 tar xvf idea.tar.gz
-rm -f idea.tar.gz
-IDEA=`ls ./`
+IDEA=`find -maxdepth 1 -mindepth 1 -type d | sed -E 's%./%%g'`
 echo $IDEA
 
+#fix idea
+cd ${IDEA}/bin
+mv idea.sh idea.sh.org
+echo '#!/bin/sh' > idea.sh
+echo 'export JAVA_HOME=`which javac | xargs readlink | xargs readlink | xargs dirname | xargs dirname`' >> idea.sh
+cat idea.sh.org >> idea.sh
+
 sudo mkdir -p /usr/lib/idea
-sudo mv /tmp/idea/${IDEA} /usr/lib/idea/
+sudo mv /tmp/idea/$IDEA /usr/lib/idea/
 sudo update-alternatives --install "/usr/bin/idea" "idea" "/usr/lib/idea/${IDEA}/bin/idea.sh" 1
 sudo update-alternatives --set idea "/usr/lib/idea/${IDEA}/bin/idea.sh"
+
+rm -f idea.tar.gz
 
 
 exit
