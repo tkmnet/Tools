dafb1b23fba25216a65aa33951e62022a89224a1
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2015-05-03 11:04:01 +0900

    update install-shortcut-idea.sh

--- install-shortcut-idea.sh
+++ install-shortcut-idea.sh
@@ -4,4 +4,4 @@ cd /usr/local/bin
 echo '#!/bin/sh' > idea
 echo 'export JAVA_HOME=`which javac | xargs readlink -f | xargs dirname | xargs dirname`' >> idea
 echo 'intellij-idea-ce-eap' >> idea
-
+chmod a+x idea
